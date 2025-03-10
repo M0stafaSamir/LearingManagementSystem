c955f30bca6606e66eb356c6d9457ff7ab863cff
Habosa162 <habosa2000@hotmail.com>
2025-03-11 01:49:10 +0200

    CourseDetails

--- LMS/Models/AppUser.cs
+++ LMS/Models/AppUser.cs
@@ -8,7 +8,6 @@ namespace LMS.Models
     {
         public string Name { get; set; }
         public string ProfileImg { get; set; }
-
         public ICollection<StudentEnrollCourse> EnrolledCourses { get; set; } = new List<StudentEnrollCourse>();
         public ICollection<Purchase> Purchases { get; set; } = new List<Purchase>();
     }
